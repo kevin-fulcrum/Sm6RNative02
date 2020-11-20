597e5ae73cdd5708eb32264fc0bbe0269d1053eb
Daniel Soto <danielsoto@Daniels-Mac.local>
2020-11-19 17:49:08 -0800

    [feature] - create & style carousel - carousel item component & implement data in App js

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  Animated,
+} from 'react-native';
+import CarouselItem from './CarouselItem';
+
+const {height, width} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  dot: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+});
+
+const Carousel = ({data}) => {
+  const scrollX = new Animated.Value(0);
+  let position = Animated.divide(scrollX, width);
+  if (data && data.length) {
+    return (
+      <View style={{flex: 0.4}}>
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => 'key' + index}
+          horizontal
+          pagingEnabled
+          scrollEnabled
+          snapToAlignment="center"
+          scrollEventThrottle={16}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator="false"
+          renderItem={(item) => {
+            return <CarouselItem item={item.item} />;
+          }}
+          onScroll={Animated.event([
+            {nativeEvent: {contentOffset: {x: scrollX}}},
+          ])}
+        />
+        <View style={styles.dot}>
+          {data.map((_, i) => {
+            let opacity = position.interpolate({
+              inputRange: [i - 1, i, i + 1],
+              outputRange: [0.3, 1, 0.3],
+              extrapolate: 'clamp',
+            });
+            return (
+              <Animated.View
+                key={i}
+                style={{
+                  opacity,
+                  height: 10,
+                  width: 10,
+                  backgroundColor: '#212121',
+                  margin: 8,
+                  borderRadius: 5,
+                }}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
+  return null;
+};
+
+export default Carousel;
---
+++ src/resource/functions/data/carouselData.js
@@ -0,0 +1,31 @@
+export const carouselData = [
+  {
+    title: 'Corfu, Greece',
+    description:
+      'Corfu is absolutely romantic and calm in the evenings, especially next to the sea. Few days ago me and my husband Pete had a chance to spend an hour in Corfu Town when sun was going down and day was slowly moving to its end. It was absolutely charming and beautiful.',
+    image:
+      'https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80',
+    id: 1,
+  },
+  {
+    title: 'Hyatt Regency Orlando, winter garden, US',
+    description: 'Woman in a summer dress',
+    image:
+      'https://images.unsplash.com/photo-1562373353-7db88744f982?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=896&q=80',
+    id: 2,
+  },
+  {
+    title: 'California, US',
+    description: 'Woman in a dress',
+    image:
+      'https://images.unsplash.com/photo-1600075110701-6ea93b9b5f70?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    id: 3,
+  },
+  {
+    title: 'Rio de Janeiro, RJ, Brasil',
+    description: ' beautiful woman in a dress.',
+    image:
+      'https://images.unsplash.com/photo-1586471176822-f1beeaa03fe4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    id: 4,
+  },
+];
