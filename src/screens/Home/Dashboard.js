ff3c0cb33d2ed5d68739665feb515abe4785460f
Daniel Soto <danielsoto@Daniels-Mac.local>
2020-11-21 06:29:02 -0800

    [feture] - create product Slider & fix style carousel & slider - implement Dashboard

--- src/screens/Home/Dashboard.js
+++ src/screens/Home/Dashboard.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
 import {carouselData} from '../../resource/functions/data/carouselData';
 import Carousel from '../../components/Carousel/Carousel';
+import ProductSlider from '../../components/ProductSlider/ProductSlider';
+import {productSliderData} from '../../resource/functions/data/productSliderData';
 
 const Dashboard = () => {
   return (
-    <View style={styles.container}>
-      <Carousel data={carouselData} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <View style={{flex: 0.5}}>
+          <Carousel data={carouselData} />
+        </View>
+        <View style={{flex: 0.35}}>
+          <Text style={styles.title}>T-Shirt</Text>
+          <ProductSlider data={productSliderData} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  text: {
-    fontSize: 40,
+  title: {
+    fontSize: 20,
+    marginTop: 10,
+    marginHorizontal: 20,
+    fontWeight: 'bold',
   },
 });
 
