4a67abdad116aaccf7bf05d98bb8e8cdef30648f
danielsotopachaqtec <daniel.soto@pachaqtec.edu.pe>
2020-11-23 21:42:38 -0500

    [feature] - setup react native maps in Android & maps ios component style

--- src/components/Maps/iOS/index.js
+++ src/components/Maps/iOS/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, SafeAreaView} from 'react-native';
 import MapView from 'react-native-maps';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  maps: {...StyleSheet.absoluteFillObject},
 });
 
 const MapIOS = () => {
   return (
-    <View style={styles.container}>
-      <MapView style={styles.container} />
-    </View>
+    <SafeAreaView>
+      <View style={styles.maps}>
+        <MapView
+          style={styles.container}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
