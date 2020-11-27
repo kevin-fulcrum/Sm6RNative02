4eafabc2df39301c6ee543f82296b6d3cf70cb83
Daniel Soto <danielsoto@Daniels-Mac.local>
2020-11-26 19:27:09 -0800

    [feature] - add stack navigator in app & install react navigation

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Dashboard from './src/screens/Home/Dashboard';
 import MapIOS from './src/components/Maps/iOS';
 import GoogleMaps from './src/components/Maps/Android';
@@ -15,18 +16,25 @@ import TrackUserLocationMap from './src/components/Maps/TrackUserLocationMap';
 import Register from './src/screens/Authentication/Register';
 import Login from './src/screens/Authentication/Login';
 import {AuthProvider} from './src/navigation/AuthProvider';
+import UseCamera from './src/screens/UseCamera';
+
+const Stack = createStackNavigator();
 const App = () => {
   return (
-    <AuthProvider>
-      <View style={styles.container}>
-        {/* <Dashboard /> */}
-        {/* <MapIOS /> */}
-        {/* <GoogleMaps /> */}
-        {/* <TrackUserLocationMap /> */}
-        {/* <Register /> */}
-        <Login />
-      </View>
-    </AuthProvider>
+    <Stack.Navigator>
+      <Stack.Screen name="Dashboard" component={Dashboard} />
+      {/* <AuthProvider> */}
+      {/* <View style={styles.container}> */}
+      {/* <Dashboard /> */}
+      {/* <MapIOS /> */}
+      {/* <GoogleMaps /> */}
+      {/* <TrackUserLocationMap /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      {/* <UseCamera /> */}
+      {/* </View> */}
+      {/* </AuthProvider> */}
+    </Stack.Navigator>
   );
 };
 
