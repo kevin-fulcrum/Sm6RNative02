24231ed3cc324a173a53ffe95cc8829c0b57cf00
danielsotopachaqtec <daniel.soto@pachaqtec.edu.pe>
2020-12-12 09:47:21 -0500

    [feature] - fix when keyboard pup up

--- src/screens/ShoppingCart/index.js
+++ src/screens/ShoppingCart/index.js
@@ -6,14 +6,17 @@ import {
   SafeAreaView,
   ScrollView,
   Platform,
+  KeyboardAvoidingView,
 } from 'react-native';
 import CartItem from '../../components/ShoppingCart/CartItem';
 import CartDetailsPayment from '../../components/ShoppingCart/CartDetailsPayment';
 import MenuFooter from '../../components/core/Menu/MenuFooter';
 import Input from '../../components/core/Form/TextInput';
-import {onChange} from 'react-native-reanimated';
 
 const styles = StyleSheet.create({
+  keyboardAvoid: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     marginHorizontal: 20,
@@ -107,70 +110,74 @@ const ShoppingCart = ({navigation, route}) => {
     navigation.navigate('PaymentMethods', params);
   };
   return (
-    <SafeAreaView style={styles.containerSafeArea}>
-      <View style={styles.container}>
-        {route.params ? (
-          <>
-            <ScrollView nestedScrollEnabled style={{flex: 0.1}}>
-              <CartItem
-                title={title}
-                image={image}
-                price={price}
-                category={category}
-                collections={collections}
-              />
-            </ScrollView>
-            <View style={styles.shippingContainer}>
-              <View style={styles.shippingContent}>
-                <View style={styles.action}>
-                  <Input
-                    label="Location"
-                    labelStyle={styles.title}
-                    vale={location}
-                    ref={inputLocation}
-                    type="name"
-                    placeholder="Lima, Peru"
-                    placeholderTextColor="#cccccc"
-                    textInputStyle={styles.textInput}
-                    maxLength={90}
-                    onChangeInput={(value) => onChange(value, 'location')}
-                  />
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={65}
+      style={styles.keyboardAvoid}>
+      <SafeAreaView style={styles.containerSafeArea}>
+        <View style={styles.container}>
+          {route.params ? (
+            <>
+              <ScrollView nestedScrollEnabled style={{flex: 0.1}}>
+                <CartItem
+                  title={title}
+                  image={image}
+                  price={price}
+                  category={category}
+                  collections={collections}
+                />
+              </ScrollView>
+              <View style={styles.shippingContainer}>
+                <View style={styles.shippingContent}>
+                  <View style={styles.action}>
+                    <Input
+                      label="Location"
+                      labelStyle={styles.title}
+                      vale={location}
+                      ref={inputLocation}
+                      type="name"
+                      placeholder="Lima, Peru"
+                      placeholderTextColor="#cccccc"
+                      textInputStyle={styles.textInput}
+                      maxLength={90}
+                      onChangeInput={(value) => onChange(value, 'location')}
+                    />
+                  </View>
+                </View>
+                <View style={styles.shippingContent}>
+                  <View style={styles.action}>
+                    <Input
+                      label="Message"
+                      labelStyle={styles.title}
+                      vale={message}
+                      ref={inputMessage}
+                      type="name"
+                      placeholder="Shipping detail"
+                      placeholderTextColor="#cccccc"
+                      textInputStyle={styles.textInput}
+                      maxLength={90}
+                      onChangeInput={(value) => onChange(value, 'message')}
+                    />
+                  </View>
                 </View>
               </View>
-              <View style={styles.shippingContent}>
-                <View style={styles.action}>
-                  <Input
-                    label="Message"
-                    labelStyle={styles.title}
-                    vale={message}
-                    ref={inputMessage}
-                    type="name"
-                    placeholder="Shipping detail"
-                    placeholderTextColor="#cccccc"
-                    textInputStyle={styles.textInput}
-                    maxLength={90}
-                    onChangeInput={(value) => onChange(value, 'message')}
-                  />
-                </View>
+              <View style={styles.detailContainer}>
+                <CartDetailsPayment
+                  price={total}
+                  shipping={parseInt(8, 10)}
+                  onPress={goToPayment}
+                  disabled={disabledButton}
+                />
               </View>
+            </>
+          ) : (
+            <View style={styles.shoppingCartContainer}>
+              <Text style={styles.textEmpty}>Shopping cart is empty</Text>
             </View>
-            <View style={styles.detailContainer}>
-              <CartDetailsPayment
-                price={total}
-                shipping={parseInt(8, 10)}
-                onPress={goToPayment}
-                disabled={disabledButton}
-              />
-            </View>
-          </>
-        ) : (
-          <View style={styles.shoppingCartContainer}>
-            <Text style={styles.textEmpty}>Shopping cart is empty</Text>
-          </View>
-        )}
-      </View>
-      <MenuFooter navigation={navigation} route={route} />
-    </SafeAreaView>
+          )}
+        </View>
+        <MenuFooter navigation={navigation} route={route} />
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
