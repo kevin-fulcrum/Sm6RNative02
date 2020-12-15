e20116cba48d8c3ea5e93576fc5e360354fcd033
Daniel Soto <danielsoto@Daniels-Mac.local>
2020-12-14 18:09:07 -0800

    [feature] - animated circular progress

--- src/components/CircularProgress/Constans.js
+++ src/components/CircularProgress/Constans.js
@@ -3,6 +3,6 @@ import {windowWidth} from '../../resource/functions/Dimensions';
 export const {PI} = Math;
 export const TAU = 2 * PI;
 export const RADIUS = windowWidth / 2;
-export const STROKE_WIDTH = 100;
+export const STROKE_WIDTH = 80;
 export const COLOR_FG = '#2CB9B0';
 export const COLOR_BG = '#ffffff';
--- src/components/CircularProgress/index.js
+++ src/components/CircularProgress/index.js
@@ -3,15 +3,18 @@ import {View, StyleSheet} from 'react-native';
 import Animated, {
   Extrapolate,
   interpolate,
+  lessThan,
   multiply,
 } from 'react-native-reanimated';
-import {PI} from './Constans';
+import {transformOrigin} from 'react-native-redash/lib/module/v1';
+import {PI, RADIUS} from './Constans';
 import HalfCircle from './HalfCircle';
 
 const CircularProgress = ({progress, bg, fg}) => {
   const theta = multiply(progress, 2 * PI);
-  const rotateTop = interpolate(theta, {
-    inputRange: [0, PI],
+  const opacity = lessThan(theta, PI);
+  const rotate = interpolate(theta, {
+    inputRange: [PI, 2 * PI],
     outputRange: [0, PI],
     extrapolate: Extrapolate.CLAMP,
   });
@@ -22,14 +25,19 @@ const CircularProgress = ({progress, bg, fg}) => {
         <Animated.View
           style={{
             ...StyleSheet.absoluteFillObject,
-            transform: [{rotate: rotateTop}],
+            transform: transformOrigin({x: 0, y: RADIUS / 2}, {rotate: theta}),
+            opacity,
           }}>
           <HalfCircle color={bg} />
         </Animated.View>
       </View>
       <View style={{transform: [{rotate: '180deg'}]}}>
         <HalfCircle color={fg} />
-        <Animated.View style={{...StyleSheet.absoluteFillObject}}>
+        <Animated.View
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            transform: transformOrigin({x: 0, y: RADIUS / 2}, {rotate}),
+          }}>
           <HalfCircle color={bg} />
         </Animated.View>
       </View>
