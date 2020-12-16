bf51e353eb8d664f965df9e0dcfd8f459fa1b15c
danielsotopachaqtec <daniel.soto@pachaqtec.edu.pe>
2020-12-15 20:55:36 -0500

    [feature] - import svg & setup svg transformer

--- src/components/core/Buttons/ButtonPayment.js
+++ src/components/core/Buttons/ButtonPayment.js
@@ -1,45 +1,40 @@
-import React from 'react'
-import { View, Image, StyleSheet } from 'react-native'
-import CircularProgress from '../../CircularProgress'
-import {
-    COLOR_BG,
-    COLOR_FG,
-  } from '../../CircularProgress/Constans';
+import React from 'react';
+import {View, Image, StyleSheet} from 'react-native';
+import CircularProgress from '../../CircularProgress';
+import {COLOR_BG, COLOR_FG} from '../../CircularProgress/Constans';
 
-  const SIZE = 150;
-  const STOKE_WIDTH = 10;
+import FingerPrint from '../../../resource/static/images/icons/fingerprint.svg';
+const SIZE = 150;
+const STOKE_WIDTH = 10;
+const ICON_SIZE = 96;
 
-  const styles = StyleSheet.create({
-      iconContainer: {
-        position: 'absolute',
-        top: STOKE_WIDTH,
-        left: STOKE_WIDTH,
-        right: STOKE_WIDTH,
-        bottom: STOKE_WIDTH,
-        backgroundColor: '#ffffff',
-        borderRadius: (SIZE - STOKE_WIDTH * 2) / 2,
-        zIndex: 100,
-      }
-  })
+const styles = StyleSheet.create({
+  iconContainer: {
+    position: 'absolute',
+    top: STOKE_WIDTH,
+    left: STOKE_WIDTH,
+    right: STOKE_WIDTH,
+    bottom: STOKE_WIDTH,
+    backgroundColor: '#ffffff',
+    borderRadius: (SIZE - STOKE_WIDTH * 2) / 2,
+    zIndex: 100,
+  },
+});
 
-const ButtonPayment = ({ progress }) => {
-    return (
+const ButtonPayment = ({progress}) => {
+  return (
     <View>
-        <CircularProgress 
-            radius={SIZE / 2}
-            {...{progress}}
-            fg={COLOR_FG}
-            bg={COLOR_BG}
-
-        />   
-            <View style={styles.iconContainer}>
-                <Image 
-                style={{ width: 80, height: 80 }}
-                source={require('../../../resource/static/images/icons/fingerprint.png')}
-                />
-            </View>
-     </View>
-    )
-}
+      <CircularProgress
+        radius={SIZE / 2}
+        {...{progress}}
+        fg={COLOR_FG}
+        bg={COLOR_BG}
+      />
+      <View style={styles.iconContainer}>
+        <FingerPrint width={ICON_SIZE} height={ICON_SIZE} fill="#2CB9B0" />
+      </View>
+    </View>
+  );
+};
 
 export default ButtonPayment;
