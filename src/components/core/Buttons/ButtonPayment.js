e12d5e880e22ded560fb674a645452faaea92aa3
Daniel Soto <danielsoto@Daniels-Mac.local>
2020-12-15 17:41:00 -0800

    [feature] - implement icon in Button Payment

--- src/components/core/Buttons/ButtonPayment.js
+++ src/components/core/Buttons/ButtonPayment.js
@@ -1,16 +1,44 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Image, StyleSheet } from 'react-native'
 import CircularProgress from '../../CircularProgress'
 import {
     COLOR_BG,
     COLOR_FG,
   } from '../../CircularProgress/Constans';
 
-const SIZE = 150;
+  const SIZE = 150;
+  const STOKE_WIDTH = 10;
+
+  const styles = StyleSheet.create({
+      iconContainer: {
+        position: 'absolute',
+        top: STOKE_WIDTH,
+        left: STOKE_WIDTH,
+        right: STOKE_WIDTH,
+        bottom: STOKE_WIDTH,
+        backgroundColor: '#ffffff',
+        borderRadius: (SIZE - STOKE_WIDTH * 2) / 2,
+        zIndex: 100,
+      }
+  })
 
 const ButtonPayment = ({ progress }) => {
     return (
-     <CircularProgress radius={SIZE / 2}  {...{progress}} fg={COLOR_FG} bg={COLOR_BG}/>   
+    <View>
+        <CircularProgress 
+            radius={SIZE / 2}
+            {...{progress}}
+            fg={COLOR_FG}
+            bg={COLOR_BG}
+
+        />   
+            <View style={styles.iconContainer}>
+                <Image 
+                style={{ width: 80, height: 80 }}
+                source={require('../../../resource/static/images/icons/fingerprint.png')}
+                />
+            </View>
+     </View>
     )
 }
 
