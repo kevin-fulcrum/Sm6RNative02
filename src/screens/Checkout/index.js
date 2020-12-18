854182b5987b7bdd2042f389035dc545e2f78d91
danielsotopachaqtec <daniel.soto@pachaqtec.edu.pe>
2020-12-17 19:59:55 -0500

    [feature] - add input field & intializing state & ref

--- src/screens/Checkout/index.js
+++ src/screens/Checkout/index.js
@@ -1,16 +1,23 @@
 import React, { useState(, useRef) } from 'react';
 import { View, StyleSheet, SafeAreaView, ScrollView, Platform } from 'react-native'
-import Buttom from '../../components/core/Buttons/Button';
+import Button from '../../components/core/Buttons/Button';
 import Input from '../../components/core/Form/TextInput';
 import { CardPayment } from '../../components/Payment/CardPayment';
 import { windowHeight, windowWidth } from '../../resource/functions/Dimensions';
+import { onChange } from 'react-native-reanimated';
 
 const styles = StyleSheet.create({
 
 });
 
 const Checkout = ({ navigation, route }) => {
-    const [cardNumber, setCardNumber] = useState('')
+    const [cardNumber, setCardNumber] = useState('');
+    const [cardHolder, setCardHolder] = useState('');
+    const [expireDate, setExpireDate ] = useState('')
+    const [cvv, setCvv] = useState('')
+    const inputExpireDate = useRef();
+    const inputCvv = useRef();
+    const inputCardHolder = useRef();
     const inputCardNumber = useRef();
     const { paymentMethods } = route.params
     return(
@@ -20,23 +27,74 @@ const Checkout = ({ navigation, route }) => {
                     <CardPayment item={paymentMethods}/>
                 </View>
                 <View style={styles.paymentContainer}>
-                    <>
-                        <View style={styles.action}>
+                    <View style={styles.action}>
+                        <Input 
+                            label='Card number'
+                            labelStyle={styles.title}
+                            value={cardNumber}
+                            ref={inputCardNumber}
+                            type='cardNumber'
+                            placeholder=''
+                            placeholderTextColor='#cccccc'
+                            textInputStyle={styles.textInput}
+                            keyboardType='numeric'
+                            maxLength={16}
+                            onChangeInput={(value)=> onChange(value, 'cardNumber')}
+                        />
+                    </View>
+                    <View style={styles.action}>
+                        <Input 
+                            label='Card holder'
+                            labelStyle={style.title}
+                            value={cardHolder}
+                            ref={inputCardHolder}
+                            type='name'
+                            placeholder=''
+                            placeholderTextColor='#cccccc'
+                            textInputStyle={styles.textInput}
+                            maxLength={50}
+                            onChangeInput={(value) => onChange(value, 'cardHolder')}
+                        />
+                    </View>
+                    <View style={styles.secureCardData}>
+                        <View style={[styles.expireDate, styles.action]}>
                             <Input 
-                                label='Card number'
+                                label='Expire Date'
                                 labelStyle={styles.title}
-                                value={cardNumber}
-                                ref={inputCardNumber}
-                                type='cardNumber'
-                                placeholder=''
+                                value={expireDate}
+                                ref={inputExpireDate}
+                                type='expireDate'
+                                placeholder='XX/XX'
                                 placeholderTextColor='#cccccc'
-                                textInputStyle={styles.textInput}
                                 keyboardType='numeric'
-                                maxLength={16}
-                                onChangeInput={(value)=> onChange(value, 'cardNumber')}
+                                textInputStyle={styles.textInput}
+                                maxLength={5}
+                                onChangeInput={(value) => onChange(value, 'expireDate')}
                             />
                         </View>
-                    </>
+                        <View style={[styles.dataCvv, styles.action]}>
+                            <Input 
+                                label='CVV'
+                                labelStyle={styles.title}
+                                value={cvv}
+                                ref={inputCvv}
+                                type='cvv'
+                                placeholder='XXX'
+                                placeholderTextColor='#cccccc'
+                                keyboardType='numeric'
+                                textInputStyle={styles.textInput}
+                                maxLength={3}
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.buyButtonContainer}>
+                    <Button
+                        onPress={sendPayment}
+                        variant="primary"
+                        label="Complete Payment"
+                        disabled={disabled}
+                    />
+                    </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
