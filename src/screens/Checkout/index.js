33dacaf61b4257880319f37c898eba40e8ca49c2
danielsotopachaqtec <daniel.soto@pachaqtec.edu.pe>
2020-12-17 20:10:52 -0500

    [fix] - miss spell import

--- src/screens/Checkout/index.js
+++ src/screens/Checkout/index.js
@@ -1,135 +1,139 @@
-import React, { useState(, useRef) } from 'react';
-import { View, StyleSheet, SafeAreaView, ScrollView, Platform } from 'react-native'
+import React, {useState, useRef} from 'react';
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Platform,
+} from 'react-native';
 import Button from '../../components/core/Buttons/Button';
 import Input from '../../components/core/Form/TextInput';
-import { CardPayment } from '../../components/Payment/CardPayment';
-import { windowHeight, windowWidth } from '../../resource/functions/Dimensions';
-import { onChange } from 'react-native-reanimated';
+import {CardPayment} from '../../components/Payment/CardPayment';
+import {windowHeight, windowWidth} from '../../resource/functions/Dimensions';
+import {onChange} from 'react-native-reanimated';
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({});
 
-});
+const Checkout = ({navigation, route}) => {
+  const [cardNumber, setCardNumber] = useState('');
+  const [cardHolder, setCardHolder] = useState('');
+  const [expireDate, setExpireDate] = useState('');
+  const [cvv, setCvv] = useState('');
+  const [disabledButton, setDisabledButton] = useState(true);
+  const inputExpireDate = useRef();
+  const inputCvv = useRef();
+  const inputCardHolder = useRef();
+  const inputCardNumber = useRef();
+  const {paymentMethods} = route.params;
 
-const Checkout = ({ navigation, route }) => {
-    const [cardNumber, setCardNumber] = useState('');
-    const [cardHolder, setCardHolder] = useState('');
-    const [expireDate, setExpireDate ] = useState('')
-    const [cvv, setCvv] = useState('')
-    const [disabledButton, setDisabledButton] = useState(true)
-    const inputExpireDate = useRef();
-    const inputCvv = useRef();
-    const inputCardHolder = useRef();
-    const inputCardNumber = useRef();
-    const { paymentMethods } = route.params;
+  const sendPayment = () => {
+    console.warn('sendPayment');
+  };
 
-    const sendPayment = () => {
-        console.warn('sendPayment')
+  const onChange = (value, type) => {
+    if (type === 'cardNumber') {
+      setCardNumber(inputCardNumber.current.state.value);
     }
-
-    const onChange = (value, type) => {
-        if(type === 'cardNumber') {
-            setCardNumber(inputCardNumber.current.state.value)
-        }
-        if(type === 'cardHolder') {
-            setCardHolder(value)
-        }
-        if(type === 'expireDate') {
-            setExpireDate(value)
-        }
-        if(type === 'cvv') {
-            setCvv(value)
-        }
-        if(
-            value.length > 0 &&
-            inputCardNumber.current.state.validate &&
-            inputCardHolder.current.state.validate &&
-            inputExpireDate.current.state.validate &&
-            inputCvv.current.state.validate
-        ){
-            setDisabledButton(false)
-        } else {
-            setDisabledButton(true);
-        }
+    if (type === 'cardHolder') {
+      setCardHolder(value);
     }
-    return(
-        <SafeAreaView style={styles.containerSafeArea}>
-            <ScrollView>
-                <View style={styles.container}>
-                    <CardPayment item={paymentMethods}/>
-                </View>
-                <View style={styles.paymentContainer}>
-                    <View style={styles.action}>
-                        <Input 
-                            label='Card number'
-                            labelStyle={styles.title}
-                            value={cardNumber}
-                            ref={inputCardNumber}
-                            type='cardNumber'
-                            placeholder=''
-                            placeholderTextColor='#cccccc'
-                            textInputStyle={styles.textInput}
-                            keyboardType='numeric'
-                            maxLength={16}
-                            onChangeInput={(value)=> onChange(value, 'cardNumber')}
-                        />
-                    </View>
-                    <View style={styles.action}>
-                        <Input 
-                            label='Card holder'
-                            labelStyle={style.title}
-                            value={cardHolder}
-                            ref={inputCardHolder}
-                            type='name'
-                            placeholder=''
-                            placeholderTextColor='#cccccc'
-                            textInputStyle={styles.textInput}
-                            maxLength={50}
-                            onChangeInput={(value) => onChange(value, 'cardHolder')}
-                        />
-                    </View>
-                    <View style={styles.secureCardData}>
-                        <View style={[styles.expireDate, styles.action]}>
-                            <Input 
-                                label='Expire Date'
-                                labelStyle={styles.title}
-                                value={expireDate}
-                                ref={inputExpireDate}
-                                type='expireDate'
-                                placeholder='XX/XX'
-                                placeholderTextColor='#cccccc'
-                                keyboardType='numeric'
-                                textInputStyle={styles.textInput}
-                                maxLength={5}
-                                onChangeInput={(value) => onChange(value, 'expireDate')}
-                            />
-                        </View>
-                        <View style={[styles.dataCvv, styles.action]}>
-                            <Input 
-                                label='CVV'
-                                labelStyle={styles.title}
-                                value={cvv}
-                                ref={inputCvv}
-                                type='cvv'
-                                placeholder='XXX'
-                                placeholderTextColor='#cccccc'
-                                keyboardType='numeric'
-                                textInputStyle={styles.textInput}
-                                maxLength={3}
-                            />
-                        </View>
-                    </View>
-                    <View style={styles.buyButtonContainer}>
-                    <Button
-                        onPress={sendPayment}
-                        variant="primary"
-                        label="Complete Payment"
-                        disabled={disabledButton}
-                    />
-                    </View>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+    if (type === 'expireDate') {
+      setExpireDate(value);
+    }
+    if (type === 'cvv') {
+      setCvv(value);
+    }
+    if (
+      value.length > 0 &&
+      inputCardNumber.current.state.validate &&
+      inputCardHolder.current.state.validate &&
+      inputExpireDate.current.state.validate &&
+      inputCvv.current.state.validate
+    ) {
+      setDisabledButton(false);
+    } else {
+      setDisabledButton(true);
+    }
+  };
+  return (
+    <SafeAreaView style={styles.containerSafeArea}>
+      <ScrollView>
+        <View style={styles.container}>
+          <CardPayment item={paymentMethods} />
+        </View>
+        <View style={styles.paymentContainer}>
+          <View style={styles.action}>
+            <Input
+              label="Card number"
+              labelStyle={styles.title}
+              value={cardNumber}
+              ref={inputCardNumber}
+              type="cardNumber"
+              placeholder=""
+              placeholderTextColor="#cccccc"
+              textInputStyle={styles.textInput}
+              keyboardType="numeric"
+              maxLength={16}
+              onChangeInput={(value) => onChange(value, 'cardNumber')}
+            />
+          </View>
+          <View style={styles.action}>
+            <Input
+              label="Card holder"
+              labelStyle={style.title}
+              value={cardHolder}
+              ref={inputCardHolder}
+              type="name"
+              placeholder=""
+              placeholderTextColor="#cccccc"
+              textInputStyle={styles.textInput}
+              maxLength={50}
+              onChangeInput={(value) => onChange(value, 'cardHolder')}
+            />
+          </View>
+          <View style={styles.secureCardData}>
+            <View style={[styles.expireDate, styles.action]}>
+              <Input
+                label="Expire Date"
+                labelStyle={styles.title}
+                value={expireDate}
+                ref={inputExpireDate}
+                type="expireDate"
+                placeholder="XX/XX"
+                placeholderTextColor="#cccccc"
+                keyboardType="numeric"
+                textInputStyle={styles.textInput}
+                maxLength={5}
+                onChangeInput={(value) => onChange(value, 'expireDate')}
+              />
+            </View>
+            <View style={[styles.dataCvv, styles.action]}>
+              <Input
+                label="CVV"
+                labelStyle={styles.title}
+                value={cvv}
+                ref={inputCvv}
+                type="cvv"
+                placeholder="XXX"
+                placeholderTextColor="#cccccc"
+                keyboardType="numeric"
+                textInputStyle={styles.textInput}
+                maxLength={3}
+              />
+            </View>
+          </View>
+          <View style={styles.buyButtonContainer}>
+            <Button
+              onPress={sendPayment}
+              variant="primary"
+              label="Complete Payment"
+              disabled={disabledButton}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Checkout;
