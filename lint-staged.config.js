3ca5166db545c422b96710201614bf69fc3ff65e
Daniel Soto <dasg.1993@gmail.comm>
2020-12-30 21:23:40 -0500

    [testing] - install & setup enzyme & jest & create button test spect component

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@ module.exports = {
     'eslint',
     // 'jest --findRelatedTests',
   ],
-  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
+  '*.+(js|jsx|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
     'prettier --write',
     'eslint --fix',
     'git add',
