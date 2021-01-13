03c03b107df2ed3775ee2c57ff1a82397cdded57
Daniel Soto <danielsoto@Daniels-Mac.local>
2021-01-12 18:29:29 -0800

    [feature] - pod install & create collections & categories reducer & combine them & setup store in app js & remove imports

--- App.js
+++ App.js
@@ -7,20 +7,21 @@
  */
 
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
 import Providers from './src/navigation/';
-import Dashboard from './src/screens/Home/Dashboard';
-import MapIOS from './src/components/Maps/iOS';
-import GoogleMaps from './src/components/Maps/Android';
-import TrackUserLocationMap from './src/components/Maps/TrackUserLocationMap';
-import Register from './src/screens/Authentication/Register';
-import Login from './src/screens/Authentication/Login';
-import {AuthProvider} from './src/navigation/AuthProvider';
-import UseCamera from './src/screens/UseCamera';
+import {Provider} from 'react-redux';
+import {ReduxNetworkProvider} from 'react-native-offline';
+import getStore from './src/redux/store';
+
+const store = getStore();
 
 const App = () => {
-  return <Providers />;
+  return (
+    <Provider store={store}>
+      <ReduxNetworkProvider>
+        <Providers />
+      </ReduxNetworkProvider>
+    </Provider>
+  );
 };
 
 export default App;
