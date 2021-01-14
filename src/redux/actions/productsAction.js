0ff5a70ad36ea16598ef0fd0b26f455dbc0869d2
Daniel Soto <danielsoto@Daniels-Mac.local>
2021-01-13 17:17:56 -0800

    [feature] - create products action

---
+++ src/redux/actions/productsAction.js
@@ -0,0 +1,31 @@
+import Actions from '../../resource/Action';
+import {getProducts} from '../../resource/database/products';
+
+const getProductsSuccess = (data) => {
+  return {
+    type: Actions.FETCHING_PRODUCTS_SUCCESS,
+    data,
+  };
+};
+
+const getProductsFailure = (errors) => {
+  return {
+    type: Actions.FETCHING_PRODUCTS_FAILURE,
+    errors,
+  };
+};
+
+const getAllProducts = () => {
+  return async (dispatch, getState) => {
+    const products = await getProducts();
+    if (products.errors) {
+      dispatch(getProductsFailure(products.errors));
+    } else {
+      dispatch(getProductsSuccess(products));
+    }
+  };
+};
+
+export default {
+  getAllProducts,
+};
