628bd9129d4d86d74a30255a6aeec53ce6c0b9d9
danielsotopachaqtec <daniel.soto@pachaqtec.edu.pe>
2021-01-18 19:37:24 -0500

    [feature] - change ip in featch api json Server

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://192.168.1.29:3000/',
+  baseURL: 'http://192.168.0.4:3000/',
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
